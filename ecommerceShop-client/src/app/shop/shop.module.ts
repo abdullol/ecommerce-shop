16842515347fdfe664f39a024a50d3397550a521
Abdullah Moeen Muhammad <58805345+abdullah-moeen@users.noreply.github.com>
2021-08-18 19:15:24 +0500

    lazy loading

--- ecommerceShop-client/src/app/shop/shop.module.ts
+++ ecommerceShop-client/src/app/shop/shop.module.ts
@@ -5,10 +5,11 @@ import { ProductItemComponent } from './product-item/product-item.component';
 import {SharedModule} from '../shared/shared.module';
 import { RouterModule } from '@angular/router';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ShopRoutingModule } from './shop-routing.module';
 
 @NgModule({
   declarations: [ShopComponent, ProductItemComponent, ProductDetailsComponent],
-  imports: [CommonModule, SharedModule, RouterModule],
+  imports: [CommonModule, SharedModule, ShopRoutingModule, RouterModule],
   exports: [ShopComponent],
 })
 export class ShopModule {}
