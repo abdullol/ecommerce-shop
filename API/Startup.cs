6dddc4bebfbb2df8fec4491cca3f098e3280cd13
Abdullah Moeen Muhammad <58805345+abdullah-moeen@users.noreply.github.com>
2021-09-11 16:33:39 +0500

    redis basket fixed client

--- API/Startup.cs
+++ API/Startup.cs
@@ -34,13 +34,12 @@ namespace API
             services.AddAutoMapper(typeof(MappingProfiles));
             services.AddSwaggerDocumentation();
 
-            services.AddSingleton<ConnectionMultiplexer>(c =>
+            services.AddSingleton<IConnectionMultiplexer>(c =>
             {
-                var configuration = ConfigurationOptions.Parse(config.GetConnectionString("Redis"), true);
+                var configuration = ConfigurationOptions.Parse(config
+                     .GetConnectionString("Redis"), true);
                 return ConnectionMultiplexer.Connect(configuration);
             });
-
-
 
             services.AddCors(opt =>
             {
