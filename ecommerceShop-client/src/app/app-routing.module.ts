6ad530a1e3f380e1f2a0bda472efd2b118c4c875
Abdullah Moeen Muhammad <58805345+abdullah-moeen@users.noreply.github.com>
2021-09-12 13:31:07 +0500

    basket empty componet

--- ecommerceShop-client/src/app/app-routing.module.ts
+++ ecommerceShop-client/src/app/app-routing.module.ts
@@ -3,12 +3,18 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home/home.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent, data: {breadcrumbs: 'Home'} },
+  { path: '', component: HomeComponent, data: { breadcrumbs: 'Home' } },
   {
     path: 'shop',
     loadChildren: () =>
       import('./shop/shop.module').then((mod) => mod.ShopModule),
     data: { breadcrumb: 'Shop' },
+  },
+  {
+    path: 'basket',
+    loadChildren: () =>
+      import('./basket/basket.module').then((mod) => mod.BasketModule),
+    data: { breadcrumb: 'Basket' },
   },
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
