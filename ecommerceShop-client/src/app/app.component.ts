91b04f0807861896a11a318513903179461f0fd8
Abdullah Moeen Muhammad <58805345+abdullah-moeen@users.noreply.github.com>
2021-10-31 16:35:04 +0500

    basket changes

--- ecommerceShop-client/src/app/app.component.ts
+++ ecommerceShop-client/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { BasketService } from './basket/basket.service';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,18 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent implements OnInit {
   title = 'ecommerceShop-client';
-  constructor() {}
-  ngOnInit(): void {}
+  constructor(private basketService: BasketService) {}
+  ngOnInit(): void {
+    const basketId = localStorage.getItem('basket_id');
+    if (basketId) {
+      this.basketService.getBasket(basketId).subscribe(
+        () => {
+          console.log('initialzize basket');
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    }
+  }
 }
--- ecommerceShop-client/src/app/basket/basket.service.ts
+++ ecommerceShop-client/src/app/basket/basket.service.ts
@@ -1,9 +1,106 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { environment } from './../../environments/environment';
+import {
+  Basket,
+  IBasket,
+  IBasketItem,
+  IBasketTotals,
+} from '../shared/models/basket';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { IProduct } from './../shared/models/product';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BasketService {
+  baseUrl = environment.apiUrl;
 
-  constructor() { }
+  private basketSource = new BehaviorSubject<IBasket>(null);
+  basket$ = this.basketSource.asObservable();
+  private basketTotalSource = new BehaviorSubject<IBasketTotals>(null);
+  basketTotal$ = this.basketTotalSource.asObservable();
+
+  constructor(private http: HttpClient) {}
+
+  getBasket(id: string) {
+    return this.http.get(this.baseUrl + 'basket?id=' + id).pipe(
+      map((basket: any) => {
+        this.basketSource.next(basket);
+        this.calculateTotals();
+      })
+    );
+  }
+
+  setBasket(basket: IBasket) {
+    return this.http.post(this.baseUrl + 'basket', basket).subscribe(
+      (response: any) => {
+        this.basketSource.next(response);
+        this.calculateTotals(); 
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  getCurrentBasketValue() {
+    return this.basketSource.value;
+  }
+
+  addItemToBasket(item: IProduct, quantity = 1) {
+    const itemToAdd: IBasketItem = this.mapProductItemToBasketItem(
+      item,
+      quantity
+    );
+    const basket = this.getCurrentBasketValue() ?? this.createBasket();
+    basket.items.push(itemToAdd);
+    basket.items = this.addOrUpdateItem(basket.items, itemToAdd, quantity);
+    this.setBasket(basket);
+  }
+
+  private addOrUpdateItem(
+    items: IBasketItem[],
+    itemToAdd: IBasketItem,
+    quantity: number
+  ): IBasketItem[] {
+    const index = items.findIndex((i) => i.id === itemToAdd.id);
+    if (index === -1) {
+      itemToAdd.quantity = quantity;
+      items.push(itemToAdd);
+    } else {
+      items[index].quantity += quantity;
+    }
+    return items;
+  }
+
+  private mapProductItemToBasketItem(
+    item: IProduct,
+    quantity: number
+  ): IBasketItem {
+    return {
+      id: item.id,
+      productName: item.name,
+      price: item.price,
+      pictureUrl: item.pictureUrl,
+      quantity,
+      brand: item.productBrand,
+      type: item.productType,
+    };
+  }
+
+  private createBasket(): IBasket {
+    const basket = new Basket();
+    localStorage.setItem('basket_id', basket.id);
+    return basket;
+  }
+
+  private calculateTotals() {
+    const basket = this.getCurrentBasketValue();
+    const shipping = 0;
+    const subtotal = basket.items.reduce((a, b) => b.price * b.quantity + a, 0);
+    const total = subtotal + shipping;
+    this.basketTotalSource.next({shipping,total, subtotal});
+  }
 }
